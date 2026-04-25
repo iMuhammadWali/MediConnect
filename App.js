43b7dd201a66ce17cfcf8510306bba7ea300366f
Muhammad Wali <mhdwali125@gmail.com>
2026-04-25 22:55:49 +0500

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Wali is the best</Text>
+        <Image 
+        source={require('./assets/MediCare_Logo.svg')}
+        style={{ width: 200, height: 200 }}
+        />
       <StatusBar style="auto" />
     </View>
   );
